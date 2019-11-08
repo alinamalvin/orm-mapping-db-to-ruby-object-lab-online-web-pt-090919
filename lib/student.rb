3f8e45b7e7b911306a7a8e780151b14afe63f75c
Alina Kornilova <alinakornilova78@gmail.com>
2019-11-08 02:47:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -95,6 +95,27 @@ end
     DB[:conn].execute(sql,num).map do |row|
       self.new_from_db(row)
     end
+    
+     def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade < 12
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 end
 
